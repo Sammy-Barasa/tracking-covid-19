fbcbba87766007aa26205251f08c232c17f3320e
Sammy-Barasa <kesammybarasa@gmail.com>
2020-05-18 00:22:07 +0300

    deploying to netlify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import AllCountries  from './components/pickCountry'
+import Graphs from './components/chart'
+import Paperdetail from './components/paper'
+import {getDailyDataGlobal} from './getApiData/'
+import { getCountryData } from "./getApiData/"
+//  const divStyle=  {
+//    display: 'flex',
+//    flexDirection: 'column',
+//    alignItems: 'center'
+//  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      country:"",
+      data:{},
+    };
+  }
+  handleChange= async (nation)=>{
+    const CountryData = await getCountryData(nation)
+    this.setState({
+      country: nation,
+      data:CountryData
+    })
+  }
+  async componentDidMount(){
+    const fetchedGlobal= await getDailyDataGlobal()
+    this.setState({data:fetchedGlobal})
+    }
+  componentDidUpdate(){
+    const checkChoice= this.state.country
+    console.log(checkChoice)
+  }
+  render() {
+    const country= this.state.country
+    const data= this.state.data
+    console.log(data)
+    return (
+      
+      <div className='container'>
+          <Paperdetail data={data}/>
+          <AllCountries handleChange={this.handleChange}/>
+          <Graphs country={country} data={data}/>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+export default App
